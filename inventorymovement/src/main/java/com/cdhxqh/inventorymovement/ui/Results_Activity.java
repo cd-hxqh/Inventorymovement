ae70771f341114d7effb7634254dc75792e48b9c
cyf12343 <1194646605@qq.com>
2015-12-17 15:14:18 +0800

    二维码扫描

--- inventorymovement/src/main/java/com/cdhxqh/inventorymovement/ui/Results_Activity.java
+++ inventorymovement/src/main/java/com/cdhxqh/inventorymovement/ui/Results_Activity.java
@@ -1,39 +1,108 @@
 package com.cdhxqh.inventorymovement.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cdhxqh.inventorymovement.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Results_Activity extends Activity {
+    private static final String TAG="Results_Activity";
+    /**标题**/
+    private TextView titleText;
+    /**返回按钮**/
+    private ImageView backImageView;
+
+    private TextView num;//编号
+    private TextView desc;//描述
+    private Button search;//查询
+    private String result;
+    private int mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_);
+
+        getData();
+        findViewById();
+        initView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_results_, menu);
+    /**
+     * 获取数据*
+     */
+    private void getData() {
+        result = getIntent().getExtras().getString("result");
+        mark = getIntent().getIntExtra("mark", 0);
+        String s = result;
+    }
+
+    private void findViewById(){
+        titleText=(TextView)findViewById(R.id.drawer_text);
+        backImageView=(ImageView)findViewById(R.id.drawer_indicator);
+        num = (TextView) findViewById(R.id.item_num_title);
+        desc = (TextView) findViewById(R.id.item_desc_title);
+        search = (Button) findViewById(R.id.search_button);
+    }
+
+    private void initView(){
+        titleText.setText(getString(R.string.search_result));
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        if(isJson(result)){
+            try {
+                JSONObject object = new JSONObject(result);
+                if(!object.has("num")||!object.has("desc")){
+                    Toast.makeText(Results_Activity.this,"查询结果无效",Toast.LENGTH_SHORT).show();
+                }else {
+                    num.setText(object.getString("num"));
+                    desc.setText(object.getString("desc"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Toast.makeText(Results_Activity.this,"查询结果无效",Toast.LENGTH_SHORT).show();
+        }
+
+        search.setOnClickListener(searchOnClickListener);
+    }
+
+    /**
+     * 判断是否是json结构
+     */
+    public static boolean isJson(String value) {
+        try {
+            new JSONObject(value);
+        } catch (JSONException e) {
+            return false;
+        }
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private View.OnClickListener searchOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Results_Activity.this,Results_searchActivity.class);
+            intent.putExtra("search_mark",mark);
+            intent.putExtra("num",num.getText().toString());
+            startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
